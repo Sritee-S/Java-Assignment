81d54c6e067f5a5c7f44dbf225433ac2b6d5f838
Sritee-S <sritee1503@gmail.com>
2024-11-10 12:01:15 +0530

    Add files via upload

---
+++ Prgm8_InvertedTriangle.java
@@ -0,0 +1,16 @@
+/*8. Write a program that takes an integer input n and prints an inverted
+triangle of numbers*/
+import java.util.Scanner;
+public class Prgm8_InvertedTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
